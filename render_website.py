f5de6fed602cb962834ea7b33569a5d38f1ee9cf
Anton Pozdnyakov <anton.pozdnyakov.i@gmail.com>
2020-08-04 17:06:16 +0300

    Drying code (steps #19,20)

--- render_website.py
+++ render_website.py
@@ -1,26 +1,32 @@
 import argparse
 import json
-import math
 import os
 from livereload import Server, shell
+from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 def on_reload():
-    create_template(template, books_description, step)
+    create_template(books_description, step)
     print('reload!')
 
-def create_template(template, books_description, step):
+def create_template(books_description, step):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
 
-    books_count = len(books_description)
-    total_pages = math.ceil(books_count/step)
-    page_numbers = list(range(1,total_pages+1))
-    page_number = 1
     folder = 'pages'
     os.makedirs(folder, exist_ok=True)
 
-    for books in range(0,books_count, step):
+    books_count = len(books_description)
+    books_perpage_catalog = list(chunked(range(0,books_count), step))
+    page_numbers = list(range(1,len(books_perpage_catalog)+1))
+
+    for kit_id, books_kit in enumerate(books_perpage_catalog):
+        page_number = kit_id+1
         rendered_page = template.render(
-            books_description=books_description[books:books+step],
+            books_description=books_description[books_kit[0]:books_kit[-1]],
             page_numbers=page_numbers,
             current_page=page_number,
             next_page=page_number+1,
@@ -30,7 +36,6 @@ def create_template(template, books_description, step):
         filename_path = os.path.join(folder, filename)
         with open(filename_path, 'w', encoding="utf8") as file:
             file.write(rendered_page)
-        page_number = page_number + 1
 
 
 if __name__ == '__main__':
@@ -38,14 +43,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Library creater (tululu.org)')
     parser.add_argument('--books', default=20, help='Books per page (default 20)')
     args = parser.parse_args()
-
     step = int(args.books)
-
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-    template = env.get_template('template.html')
 
     try:
         with open("books_description.json", "r") as my_file:
@@ -53,10 +51,11 @@ if __name__ == '__main__':
     except FileNotFoundError:
         print('File "books_description.json" does not exist')
         exit()
-    books_description = json.loads(books_description_json)
 
-    create_template(template, books_description, step)
+    books_description = json.loads(books_description_json)
+    create_template(books_description, step)
 
     server = Server()
     server.watch('template.html', on_reload)
     server.serve(root='.')
+
